113b5c7ad305883bea429e6e9f9652399ad3298e
Sam Hollings <{31413973}+{Naawww}@users.noreply.github.com>
2021-08-18 08:22:56 +0100

    Updated the environment.yml to have the libraries we will need for maps, and also Streamlit. Also added the PHE covid19 API library.
    Also added local authority boundary file and created a nascent geog_data.py file to later on collect that data from the geoportal.

---
+++ geog_data.py
@@ -0,0 +1,5 @@
+# local authority district shapefiles
+# https://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2020-uk-bgc/
+
+# on the geoportal website, just use the menus, way better than using the search
+# https://geoportal.statistics.gov.uk/
