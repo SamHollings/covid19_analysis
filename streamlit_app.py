8434cd09d8524c5a3b6063d3549c1bd133c20dca
Sam Hollings <{31413973}+{Naawww}@users.noreply.github.com>
2021-08-19 14:00:38 +0100

    Added the regional cases figure.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,19 @@ import plotly.express as px
 
 data_pack = covid_data.get_data()
 
-df = pd.concat([data_pack['uk_nhse_feed'],
+st.write('# COVID-19 Dashboard')
+
+df_uk_data = pd.concat([data_pack['uk_nhse_feed'],
                 data_pack['england_nhse_feed']],axis=0)#.set_index(['areatype', 'date', 'name'])
 
-fig = px.line(df, x="date", y="newPillarTwoTestsByPublishDate",
+fig = px.line(df_uk_data, x="date", y="newPillarTwoTestsByPublishDate",
               color='areatype' ,
               title='Pillar 2 tests by published date')
 
-st.write('# COVID-19 Dashboard')
 st.write(fig)
 
+# region cases graph
+df_regions = data_pack['region_nhse_feed'].query("date > '2020-04-10'")
+fig_region_cases = px.line(df_regions, x='date', y='newCasesByPublishDate', color='name', title='newCasesByPublishDate per Region')
+
+st.write(fig_region_cases)
